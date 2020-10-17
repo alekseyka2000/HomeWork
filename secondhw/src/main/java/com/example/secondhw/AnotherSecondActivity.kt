bfe87e5dd88e9c32be651ef037337b7ade02c466
Aleksey <Alekseyka2000@gmail.com>
2020-10-17 17:22:33 +0300

    Add another activity

--- secondhw/src/main/java/com/example/secondhw/AnotherSecondActivity.kt
+++ secondhw/src/main/java/com/example/secondhw/AnotherSecondActivity.kt
@@ -7,10 +7,10 @@ import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_second.*
+import java.util.function.IntUnaryOperator
 
 class AnotherSecondActivity : AppCompatActivity(){
     private lateinit var set: MutableSet<Int>
-    private lateinit var intentForRes: Intent
 
     private val clickListener = View.OnClickListener { view ->
         when (view.id) {
@@ -23,42 +23,26 @@ class AnotherSecondActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
-        intentForRes = Intent(this, MainActivity::class.java)
 
-        if (intent.getIntArrayExtra("set")?.toSet()?.isNotEmpty()!!) {
-            set = intent.getIntArrayExtra("set")?.toSet() as MutableSet<Int>
-        }else{
-            Log.d("Message", "Empty set")
-            finish()
-        }
-
-        averageButton.setOnClickListener(clickListener)
+        /*averageButton.setOnClickListener(clickListener)
         divideTheSetInHalfButton.setOnClickListener(clickListener)
-        sumOfAllNumbersButton.setOnClickListener(clickListener)
+        sumOfAllNumbersButton.setOnClickListener(clickListener)*/
     }
 
     private fun averageSet(set: MutableSet<Int>) {
-        val average = set.average()
+        //val average = set.average()
         Log.d("Message", "Average: ")
-        intentForRes.putExtra("Res", average)
-        setResult(Activity.RESULT_OK, intentForRes)
         finish()
     }
 
     private fun divideSet(set: MutableSet<Int>) {
-        val res = set.divide()
-        if (res!= 0.0) {
-            intentForRes.putExtra("Res", res)
-            setResult(Activity.RESULT_OK, intentForRes)
-        }
+        //val res = set.divide()
         finish()
     }
 
     private fun sumOfAllNumbersOfSet(set: MutableSet<Int>) {
-        val sum = set.sum().toDouble()
+        //val sum = set.sum().toDouble()
         Log.d("Message", "Sum: ")
-        intentForRes.putExtra("Res", sum)
-        setResult(Activity.RESULT_OK, intentForRes)
         finish()
     }
 }
