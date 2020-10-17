452844e3097e5e6f9459d7259f16ecc7943e7988
Aleksey <Alekseyka2000@gmail.com>
2020-10-17 12:04:24 +0300

    add listener

--- secondhw/src/main/java/com/example/secondhw/MainActivity.kt
+++ secondhw/src/main/java/com/example/secondhw/MainActivity.kt
@@ -1,21 +1,39 @@
 package com.example.secondhw
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 import java.lang.Math.random
 
 class MainActivity : AppCompatActivity() {
 
-    var setNumbers: MutableSet<Int> = mutableSetOf()
+    private var setNumbers: MutableSet<Int> = mutableSetOf()
+
+    private val clickListener = View.OnClickListener { view ->
+        when (view.id) {
+            R.id.averageButton -> startSecondActivity()
+            R.id.divideTheSetInHalfButton -> startSecondActivity()
+            R.id.sumOfAllNumbersButton -> startSecondActivity()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        for (members in 1..(8*random()).toInt()){
-           setNumbers.add((random()*9).toInt())
+        for (members in 1..(8 * random()).toInt()) {
+            setNumbers.add((random() * 9).toInt())
         }
         Log.d("Messages", "Set: ${setNumbers.toString()}")
+
+        averageButton.setOnClickListener(clickListener)
+        divideTheSetInHalfButton.setOnClickListener(clickListener)
+        sumOfAllNumbersButton.setOnClickListener(clickListener)
+    }
+
+    private fun startSecondActivity() {
+
     }
 }
