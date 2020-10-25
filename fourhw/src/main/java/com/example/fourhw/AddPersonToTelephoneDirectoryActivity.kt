491d8e4f21d3e8c7a17e0d84c0470e919d30ea28
Aleksey Yarakhovich <Alekseyka2000@gmail.com>
2020-10-26 01:08:28 +0300

    add activity for add person to telephone directory

--- fourhw/src/main/java/com/example/fourhw/AddPersonToTelephoneDirectoryActivity.kt
+++ fourhw/src/main/java/com/example/fourhw/AddPersonToTelephoneDirectoryActivity.kt
@@ -2,10 +2,34 @@ package com.example.fourhw
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_add_person_to_telefone_directory.rbg
+import kotlinx.android.synthetic.main.activity_add_person_to_telefone_directory.emailEditText
+import kotlinx.android.synthetic.main.activity_add_person_to_telefone_directory.phoneEditText
+import kotlinx.android.synthetic.main.activity_add_person_to_telefone_directory.radio1
+import kotlinx.android.synthetic.main.activity_add_person_to_telefone_directory.radio2
 
 class AddPersonToTelephoneDirectoryActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_person_to_telefone_directory)
+
+        rbg.setOnCheckedChangeListener { _, checkedId ->
+            when (checkedId) {
+                R.id.radio1 -> {
+                    radio1.isChecked = true
+                    radio2.isChecked = false
+                    phoneEditText.visibility = View.VISIBLE
+                    emailEditText.visibility = View.INVISIBLE
+                }
+                R.id.radio2 -> {
+                    radio1.isChecked = false
+                    radio2.isChecked = true
+                    phoneEditText.visibility = View.INVISIBLE
+                    emailEditText.visibility = View.VISIBLE
+                }
+            }
+        }
     }
 }
--- fourhw/src/main/java/com/example/fourhw/MainActivity.kt
+++ fourhw/src/main/java/com/example/fourhw/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.fourhw
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.buttonAdd
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        buttonAdd.setOnClickListener{
+            startActivity(Intent(this,AddPersonToTelephoneDirectoryActivity::class.java))
+        }
     }
 }
