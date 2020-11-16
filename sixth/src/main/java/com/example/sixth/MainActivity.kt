1b01a68ac8dfd47b469ef3c87cddcefae34716d4
Aleksey Yarakhovich <Alekseyka2000@gmail.com>
2020-11-17 00:24:12 +0300

    add dialog builder

--- sixth/src/main/java/com/example/sixth/MainActivity.kt
+++ sixth/src/main/java/com/example/sixth/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.sixth.databinding.ActivityMainBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class MainActivity : AppCompatActivity(), CellClickListener {
 
@@ -25,10 +26,30 @@ class MainActivity : AppCompatActivity(), CellClickListener {
             adapter = FileListAdapter()
             layoutManager = LinearLayoutManager(this@MainActivity)
         }
+
+        val dialogEditText = this.layoutInflater.inflate(R.layout.dialog_edittext, null)
+        binding.addFileButton.setOnClickListener {
+            MaterialAlertDialogBuilder(this@MainActivity, R.style.AppTheme)
+                .setTitle(resources.getString(R.string.add_file))
+                .setMessage(resources.getString(R.string.add_file))
+                .setView(dialogEditText)
+                .setPositiveButton(resources.getString(R.string.positive_button_text)) { _, _ ->
+                    Toast.makeText(this@MainActivity, "File was created", Toast.LENGTH_SHORT).show()
+                }
+                .setNegativeButton(resources.getString(R.string.negative_button_text)) { _, _ ->
+                    Toast.makeText(
+                        this@MainActivity,
+                        "File creation was canceled",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                .create()
+                .show()
+        }
     }
 
     override fun onCellClickListener(name: String) {
-        Toast.makeText(this, "file", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "file $name was clicked", Toast.LENGTH_SHORT).show()
     }
 
     inner class FileListAdapter : RecyclerView.Adapter<FileListAdapter.FileViewHolder>() {
